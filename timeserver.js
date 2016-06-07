2c10010533599a39ecda7d99347c574e9477132b
arshad khan <arshdkhn1@gmail.com>
2016-06-07 13:42:13 +0000

    adding time server js file

---
+++ timeserver.js
@@ -0,0 +1,12 @@
+var net = require('net');
+var date = new Date();
+var server = net.createServer(function(socket){
+    var str = "";
+    str = date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1) )  + "-" + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate() ) + " ";
+    str += (date.getHours() < 10 ? "0" + date.getHours() : date.getHours() ) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes() );
+    socket.write(str);
+    socket.end("");
+}).on('error', function(){
+    console.log("some error happened while creating server");
+});
+server.listen(process.argv[2]);
