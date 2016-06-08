e7e2193e5edee77a0a240afe2c91a0ce61dc899f
Arshad Khan <arshdkhn1@gmail.com>
2016-06-08 16:03:43 +0530

    Update timeserver.js

--- timeserver.js
+++ timeserver.js
@@ -4,8 +4,7 @@ var server = net.createServer(function(socket){
     var str = "";
     str = date.getFullYear() + "-" + (date.getMonth() < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1) )  + "-" + (date.getDate() < 10 ? "0" + date.getDate() : date.getDate() ) + " ";
     str += (date.getHours() < 10 ? "0" + date.getHours() : date.getHours() ) + ":" + (date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes() );
-    socket.write(str);
-    socket.end("");
+    socket.end(str);
 }).on('error', function(){
     console.log("some error happened while creating server");
 });
